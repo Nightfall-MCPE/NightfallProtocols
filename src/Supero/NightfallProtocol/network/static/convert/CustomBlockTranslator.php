83064419ae7ab12692ee65221ca5f6c4f7face04
Superomarking <imposterlandsofficial@gmail.com>
2024-08-20 21:30:56 +0300

    Item/Block translations

--- src/Supero/NightfallProtocol/network/static/convert/CustomBlockTranslator.php
+++ src/Supero/NightfallProtocol/network/static/convert/CustomBlockTranslator.php
@@ -53,6 +53,10 @@ class CustomBlockTranslator
             self::CANONICAL_BLOCK_STATES_PATH => "-1.20.60",
             self::BLOCK_STATE_META_MAP_PATH => "-1.20.60",
         ],
+        CustomProtocolInfo::PROTOCOL_1_20_50 => [
+            self::CANONICAL_BLOCK_STATES_PATH => "-1.20.50",
+            self::BLOCK_STATE_META_MAP_PATH => "-1.20.50",
+        ],
     ];
 
 
