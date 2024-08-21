389c45dbe85395d0f6abfabad5a1496e0be6ba04
Superomarking <imposterlandsofficial@gmail.com>
2024-08-21 05:54:57 +0300

    Society

--- src/Supero/NightfallProtocol/network/packets/ItemStackRequestPacket.php
+++ src/Supero/NightfallProtocol/network/packets/ItemStackRequestPacket.php
@@ -4,20 +4,27 @@ namespace Supero\NightfallProtocol\network\packets;
 
 use pocketmine\network\mcpe\protocol\ItemStackRequestPacket as PM_Packet;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+use pocketmine\network\mcpe\protocol\types\inventory\stackrequest\ItemStackRequest;
 use Supero\NightfallProtocol\network\packets\types\inventory\CustomItemStackRequest;
 
 class ItemStackRequestPacket extends PM_Packet {
 
-    public array $requests;
+    /** @var ItemStackRequest[] */
+    private array $requests;
 
     /**
      * @generate-create-func
+     * @param $requests ItemStackRequest[]
      */
     public static function createPacket(array $requests) : self {
         $result = new self;
         $result->requests = $requests;
         return $result;
     }
+
+    /** @return ItemStackRequest[] */
+    public function getRequests() : array{ return $this->requests; }
+
     protected function decodePayload(PacketSerializer $in) : void {
         $this->requests = [];
         for($i = 0, $len = $in->getUnsignedVarInt(); $i < $len; ++$i){
