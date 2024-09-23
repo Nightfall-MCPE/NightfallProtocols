4b08b2ae7b03f61e986b9155a5fa91a7a525f36b
VennV <111500380+VennDev@users.noreply.github.com>
2024-09-23 11:37:50 +0700

    Update php-cs-fixer.dist.php

--- php-cs-fixer.dist.php
+++ php-cs-fixer.dist.php
@@ -2,27 +2,13 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
-use PhpCsFixer\Fixer\Internal\ConfigurableFixerTemplateFixer;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 return (new Config())
-    ->setParallelConfig(ParallelConfigFactory::detect()) // @TODO 4.0 no need to call this manually
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRiskyAllowed(true)
-    ->registerCustomFixers([
-        new ConfigurableFixerTemplateFixer(),
-    ])
     ->setRules([
         '@PHP74Migration' => true,
         '@PHP74Migration:risky' => true,
