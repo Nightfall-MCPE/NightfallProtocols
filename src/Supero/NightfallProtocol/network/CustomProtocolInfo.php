cbd9f858b19f7be03195acd5f54fbcd19672f8a0
Vega Nicholas S. <veganicholas123@gmail.com>
2024-10-23 20:10:59 +0700

    Add 1.21.40 support

--- src/Supero/NightfallProtocol/network/CustomProtocolInfo.php
+++ src/Supero/NightfallProtocol/network/CustomProtocolInfo.php
@@ -6,10 +6,11 @@ namespace Supero\NightfallProtocol\network;
 
 class CustomProtocolInfo {
 
-	public const CURRENT_PROTOCOL = self::PROTOCOL_1_21_30;
+	public const CURRENT_PROTOCOL = self::PROTOCOL_1_21_40;
 
 	public const ACCEPTED_PROTOCOLS = [
 		self::CURRENT_PROTOCOL,
+		self::PROTOCOL_1_21_30,
 		self::PROTOCOL_1_21_20,
 		self::PROTOCOL_1_21_2,
 		self::PROTOCOL_1_21_0,
@@ -26,6 +27,7 @@ class CustomProtocolInfo {
 		self::CURRENT_PROTOCOL,
 	];
 
+	public const PROTOCOL_1_21_40 = 748;
 	public const PROTOCOL_1_21_30 = 729;
 	public const PROTOCOL_1_21_20 = 712;
 	public const PROTOCOL_1_21_2 = 686;
--- src/Supero/NightfallProtocol/network/static/convert/CustomBlockTranslator.php
+++ src/Supero/NightfallProtocol/network/static/convert/CustomBlockTranslator.php
@@ -37,6 +37,10 @@ class CustomBlockTranslator
 			self::CANONICAL_BLOCK_STATES_PATH => "",
 			self::BLOCK_STATE_META_MAP_PATH => "",
 		],
+		CustomProtocolInfo::PROTOCOL_1_21_30 => [
+			self::CANONICAL_BLOCK_STATES_PATH => "", //TODO: wait for PMMP to officially support version v1.21.40
+			self::BLOCK_STATE_META_MAP_PATH => "", //TODO: wait for PMMP to officially support version v1.21.40
+		],
 		CustomProtocolInfo::PROTOCOL_1_21_20 => [
 			self::CANONICAL_BLOCK_STATES_PATH => "-1.21.20",
 			self::BLOCK_STATE_META_MAP_PATH => "-1.21.20",
