dd51ed9ad775ff5b71e93675cf65a2da6d04b4f3
Devoo <fired9229@gmail.com>
2025-06-09 17:42:57 +0700

    Update 1.21.80

--- src/Supero/NightfallProtocol/network/static/convert/CustomItemTypeDictionaryFromDataHelper.php
+++ src/Supero/NightfallProtocol/network/static/convert/CustomItemTypeDictionaryFromDataHelper.php
@@ -28,6 +28,7 @@ class CustomItemTypeDictionaryFromDataHelper
 {
 	private const PATHS = [
 		CustomProtocolInfo::CURRENT_PROTOCOL => "",
+	        CustomProtocolInfo::PROTOCOL_1_21_70 => "-1.21.70",
 		CustomProtocolInfo::PROTOCOL_1_21_60 => "-1.21.60",
 		CustomProtocolInfo::PROTOCOL_1_21_50 => "-1.21.50",
 		CustomProtocolInfo::PROTOCOL_1_21_40 => "-1.21.40",
